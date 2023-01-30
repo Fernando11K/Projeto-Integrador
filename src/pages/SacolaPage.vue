fe7aa54943a6eb2cc4fb736de18106bb651babf9
Fernando11000 <fernandoferreira10@hotmail.com.br>
2023-01-29 21:03:16 -0300

    Pequenas alterações carrinho

--- src/pages/SacolaPage.vue
+++ src/pages/SacolaPage.vue
@@ -1,51 +1,34 @@
 <template>
-  <q-page
-    class="window-height window-width row"
-    style="background: #464646;"
-  >
-    <div class="column q-pa-lg">
-      <div class="row">
-        <q-card flat class="shadow-24" style="width:950px;height:590px;">
+  <q-page class="window-height window-width row bg-grey-8">
+    <section class="row justify-between col-12">
+        <q-card flat class="shadow-24 col-8  ">
           <q-card-section class="bg-grey-10">
-            <div><h4 class="text-h5 text-white q-my-md"> 
+            <div>
+              <h4 class="text-h5 text-white q-my-md"> 
               <q-btn round color="orange" icon="shopping_cart" />  Sacola</h4>
-            <div class="absolute-bottom-right q-pr-md" >             
-            </div>
             </div>
           </q-card-section>
           
-          <ul>
-  <li>Lorem</li> 
-  <li>Ipsum</li>
-  <li>Abc</li>
-</ul>  
-           
+            <ul>
+              <li>Lorem</li> 
+              <li>Ipsum</li>
+              <li>Abc</li>
+            </ul>      
         </q-card>
-      </div>
-    </div>
-    <div class="column q-pa-lg">
-      <div class="row">
-        <q-card square class="shadow-24" style="width:300px;height:300px;">
-          <q-card-section class="bg-grey-10">
-            <h4 class="text-h5 text-white q-my-md">Total de itens</h4>
-            <div class="absolute-bottom q-pr-md" >
-        
-            </div>
+    
+        <q-card square class="shadow-24 col-3 q-mr-xl  " style="min-width:300px;max-height:300px;">
+          <q-card-section class="bg-grey-10 " >
+            <h4 class="text-h5 text-white  text-center">Total de itens para reserva</h4>
           </q-card-section>
+            <q-card-section>
+             Resumo do pedido
+      </q-card-section>
           
-          <q-card-section>
-            
-        
-          </q-card-section>
-          <q-card-actions class="q-px-lg">
-            <q-btn unelevated size="lg" color="orange" class="full-width text-white" label="Confirmar Pedido" />
+          <q-card-actions class="q-pt-sm ">
+            <q-btn unelevated size="lg" color="orange" class="full-width text-white" label="Confirmar Rerserva" />
           </q-card-actions>
-          <q-card-section class="text-center q-pa-sm">
-            
-          </q-card-section>
         </q-card>
-      </div>
-    </div>
+     </section>
   </q-page>
 </template>
 
