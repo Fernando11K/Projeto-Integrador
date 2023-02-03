03e5abe3c7fd9632f89ca0a9daf7fa56b197d097
Fernando11000 <fernandoferreira10@hotmail.com.br>
2023-02-03 00:53:39 -0300

    Commit automático

---
+++ executacommit.sh
@@ -0,0 +1,11 @@
+# Variáveis para armazenar informações sobre o commit
+message="Commit automático"
+
+# Adicionar todos os arquivos à área de stage
+git add .
+
+# Fazer o commit com a mensagem especificada
+git commit -m "$message"
+
+# Enviar as alterações para o repositório remoto
+git push origin master
